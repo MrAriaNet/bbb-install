c45a1c3d7b814532e599182d0cab6eb55284a8a4
Aria <47712536+MrAriaNet@users.noreply.github.com>
2025-07-16 12:26:08 +0330

    Update bbb-install.sh
    set iran mirror

--- bbb-install.sh
+++ bbb-install.sh
@@ -120,7 +120,7 @@ HERE
 
 main() {
   export DEBIAN_FRONTEND=noninteractive
-  PACKAGE_REPOSITORY=ubuntu.bigbluebutton.org
+  PACKAGE_REPOSITORY=bbb.mobinhost.com
   LETS_ENCRYPT_OPTIONS=(--webroot --non-interactive)
   SOURCES_FETCHED=false
   GL3_DIR=~/greenlight-v3
